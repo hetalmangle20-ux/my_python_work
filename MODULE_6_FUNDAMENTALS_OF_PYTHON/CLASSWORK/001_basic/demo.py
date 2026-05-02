53e854d58a37f45676a7b5c3bab8e797a8f12c89
HETAL MANGLE <hetalmangle20@gmail.com>
2026-05-02 10:44:37 +0530

    mywork

---
+++ MODULE_6_FUNDAMENTALS_OF_PYTHON/CLASSWORK/001_basic/demo.py
@@ -0,0 +1,29 @@
+# print("HELLO WORLD")
+# print("HELLO PYTHON")
+# print("""HELLO 
+#       TO THE 
+#       WORLD
+#       OF PYTHON""")
+
+
+
+
+# a = input("enter number: ")
+# print(a)
+
+
+# HELLO /PYTHON
+
+
+
+print("python","java","php","android",sep=" -|- ")
+
+name = "HETAL"
+email="hetalmangle20@gmail.com"
+age =22
+#print("my name is {1} and email is {2} and age{3}".format(name,email,age))
+
+print(f"my name is {name} and email is {email}")
+
+print(r"Hello\b \\\" \t \n python")
+
